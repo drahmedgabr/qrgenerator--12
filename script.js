2b54bc88b7946502df18077aeb60e34311524dfb
drahmedgabr <ahmadal4ever@gmail.com>
2024-09-14 17:04:56 +0300

    finish

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const canvas = document.getElementById("qrCode");
 const userInput = document.getElementById("userInput");
+const qrText = document.getElementById("qrText");
+
 
 function generateCode() {
-    var qr = new QRious({
-        element: canvas,
-        value: userInput.value,
-    });
+    if (userInput.value == "") {
+        alert("Please enter text to convert");
+    } else {
+        var qr = new QRious({
+            element: canvas,
+            value: userInput.value,
+            size: 200,
+            foreground: "purple",
+        });
+        canvas.style.display = "inline-block";
+        qrText.innerText = userInput.value;
+        userInput.value = "";
+        canvas.scrollIntoView();
+    }
+   
 }
